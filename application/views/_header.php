badef1649e3cde5bbd2b8b388eae9f4271a72bdf
BallChen <BallChen@BallChendeMacBook-Pro.local>
2014-05-31 15:33:09 +0800

    add _footer/adjust view

--- application/views/_header.php
+++ application/views/_header.php
@@ -2,6 +2,20 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+<!-- CSS -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/">
+    <link href="<?php echo base_url('css/bootstrap.min.css');?>" rel="stylesheet" media="screen">
+    <link href="<?php echo base_url('css/bootstrap-responsive.css');?>" rel="stylesheet" media="screen">
+    <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" media="screen" href="http://silviomoreto.github.io/bootstrap-select/stylesheets/bootstrap-select.css">
+    
+<!-- JS -->
+
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <script src="<?php echo base_url('js/jquery.js'); ?>"></script>
+    <script src="<?php echo base_url('js/bootstrap.min.js'); ?>"></script>
+    
     <title>
     <?php
         if(isset($pageTitle)){
@@ -13,3 +27,4 @@
     ?>
     </title>
 </head>
+<body>
