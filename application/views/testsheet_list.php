39539cc72a8cc0425cd198dc4dbd09c6ee73dc93
johnny12728 <b00705023@ntu.edu.tw>
2014-06-06 12:52:08 +0800

    fix bug of view testsheet_list

--- application/views/testsheet_list.php
+++ application/views/testsheet_list.php
@@ -4,26 +4,31 @@
 <!DOCTYPE html>
 <body>
 	<table>
+	<?php $cur = NULL;?>
+	<?php for($i=0, $size = count($info); $i<$size; $i++){ ?>
+	<?php if( $cur != $info[$i]->subject ) { ?>
+		</table>
+		<table>
+		<tr><?php echo ($cur = $info[$i]->subject);?></tr>
 		<tr>
-			<td><?php echo "科目"?></td>
 			<td><?php echo "學年度"?></td>
 			<td><?php echo "學期"?></td>
 			<td><?php echo "第幾次段考"?></td>
 			<td><?php echo "教授"?></td>
 		</tr>
-		
-		<?php foreach($info as $rows){
-				$times = $rows->timeid / 10;
-				$semester = ($rows->timeid / 100 - $times) / 10;
-				$year = ($rows->timeid - $semester*10 - $times) / 100;?>
+		<?php }?>
+		<?php  
+			$year = floor($info[$i]->timeid/ 100);
+			$semester = floor($info[$i]->timeid / 10 - $year*10);
+			$times = floor($info[$i]->timeid - $year*100 - $semester * 10 );?>
 			<tr>
-				<td><?php echo $rows->subject?></td>
 				<td><?php echo $year?></td>
 				<td><?php echo $semester?></td>
 				<td><?php echo $times?></td>
-				<td><?php echo $rows->professor?></td>
+				<td><?php echo $info[$i]->professor?></td>
 				
 			</tr>
-		<?php }?>
+		<?php  
+		}?>
 	</table>
 </body>
